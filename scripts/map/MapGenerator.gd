9b89fbf6d9f1b95f7816eda18138e1caaab66261
Di4ik3r <di4iker@gmail.com>
2020-05-22 14:10:55 +0300

    map export for blocks

--- scripts/map/MapGenerator.gd
+++ scripts/map/MapGenerator.gd
@@ -9,11 +9,11 @@ signal refresh_ui(values)
 
 export(NodePath) var TerrainPath
 export(NodePath) var TreesPath
+export(Resource) var MapVars = preload("res://resources/map/MapVars.tres")
 
 var Terrain: MultiMeshInstance
 var Trees: MultiMeshInstance
-
-export(Resource) var MapVars = preload("res://resources/map/MapVars.tres")
+var map_blocks: Array
 
 func _init() -> void:
 	MapVars.connect("refresh_ui", self, "_on_refresh_ui")
@@ -33,6 +33,9 @@ var min_height
 var max_height
 func _generate(isTragic: bool) -> void:
 	var forest_blocks = Array()
+	var blocks = Array()
+	var tress = Array()
+	
 	if isTragic or not min_height or not max_height:
 		min_height = 999
 		max_height = -999
@@ -50,21 +53,27 @@ func _generate(isTragic: bool) -> void:
 	var instance: int = 0
 	for a in range(0, MapVars.a_side):
 		for b in range(0, MapVars.b_side):
+			var block_color: Color
+			
 			var noise = MapVars.noise.get_noise_2d(a, b) * MapVars.NOISE_MULTIPLIER
 			var height_compare = noise + MapVars.HEIGHT_ADDITION
 			var height = MapVars.BLOCK_HEIGHT_MULTIPLIER * MapVars.noise.get_noise_2d(a, b)
 
 			if height_compare < MapVars.WATER_COMPARISON:
-				height = _generate_water(noise, instance)
+				height = _generate_water(noise, instance, block_color)
 			elif height_compare < MapVars.SAND_COMPARISON:
-				_generate_sand(noise, instance)
+				_generate_sand(noise, instance, block_color)
 			elif height_compare > 7 - MapVars.MOUNTAIN_COMPARISON && MapVars.generate_mountain:
-				height = _generate_mountain(min_height, height, noise, instance)
+				height = _generate_mountain(min_height, height, noise, instance, block_color)
 			elif height_compare > 8 - MapVars.forest_comparison:
 				forest_blocks.append(_generate_tree(a, b, min_height, height, noise, instance))
 			else:
-				_generate_grass(noise, instance)
-			_place_block(a, b, min_height, height, noise, instance)
+				_generate_grass(noise, instance, block_color)
+			
+			blocks.append({
+				"transform": _place_block(a, b, min_height, height, noise, instance),
+				"color": block_color,
+			})
 			instance += 1
 
 	var count: int = forest_blocks.size()
@@ -83,22 +92,24 @@ func _generate(isTragic: bool) -> void:
 		Trees.multimesh.set_instance_color(i, Color(0.2, 0.1, 0))
 		Trees.multimesh.set_instance_transform(i, 
 			Transform(Basis(), vector))
+	
+	map_blocks = blocks
 #	pass
 
-# ########################################################################################### BLOCK GEN
-func _generate_water(noise: float, instance: int) -> float:
-	var color: = Color(MapVars.COLOR_WATER.to_html()).linear_interpolate(MapVars.COLOR_WATER_INTERPOLATION,
+# ######################################################################################## BLOCK GEN
+func _generate_water(noise: float, instance: int, color: Color) -> float:
+	color = Color(MapVars.COLOR_WATER.to_html()).linear_interpolate(MapVars.COLOR_WATER_INTERPOLATION,
 		Tools.noise_float_lerp(noise) * noise + 1)
 	Terrain.multimesh.set_instance_color(instance, color)
 	return MapVars.WATER_HEIGHT - MapVars.BLOCK_HEIGHT_MULTIPLIER / 1.89
 
-func _generate_sand(noise: float, instance: int) -> void:
-	var color: = Color(MapVars.COLOR_SAND.to_html()).linear_interpolate(MapVars.COLOR_SAND_INTERPOLATE,
+func _generate_sand(noise: float, instance: int, color: Color) -> void:
+	color = Color(MapVars.COLOR_SAND.to_html()).linear_interpolate(MapVars.COLOR_SAND_INTERPOLATE,
 		Tools.noise_float_lerp(noise) + 0.2)
 	Terrain.multimesh.set_instance_color(instance, color)
 
-func _generate_mountain(min_height: float, height: float, noise:float, instance: int) -> float:
-	var color: = Color(MapVars.COLOR_MOUNTAIN.to_html()).linear_interpolate(MapVars.COLOR_MOUNTAIN_INTERPOLATION,
+func _generate_mountain(min_height: float, height: float, noise:float, instance: int, color: Color) -> float:
+	color = Color(MapVars.COLOR_MOUNTAIN.to_html()).linear_interpolate(MapVars.COLOR_MOUNTAIN_INTERPOLATION,
 		Tools.noise_float_lerp(noise) * 5 - 10)
 	height *= noise * MapVars.MOUNTAIN_COMPARISON / 1.5
 	height -= MapVars.BLOCK_HEIGHT_MULTIPLIER / 2
@@ -122,19 +133,19 @@ func _generate_tree(a: int, b: int, min_height: float, height: float, noise: flo
 
 	return pos
 
-func _generate_grass(noise, instance) -> void:
-	var color: = Color(MapVars.COLOR_GRASS.to_html()).linear_interpolate(MapVars.COLOR_GRASS_INTERPOLATION,
+func _generate_grass(noise, instance, color: Color) -> void:
+	color = Color(MapVars.COLOR_GRASS.to_html()).linear_interpolate(MapVars.COLOR_GRASS_INTERPOLATION,
 		Tools.noise_float_lerp(noise) )
 	Terrain.multimesh.set_instance_color(instance, color)
 
-func _place_block(a: int, b: int, min_height:float, height: float,  noise: float, instance: int) -> void:
+func _place_block(a: int, b: int, min_height:float, height: float,  noise: float, instance: int) -> Transform:
 	var pos = Vector3(a, (height + abs(min_height) + MapVars.CUBE_SIZE / 2), b)
 	var basis = Basis()
 	var calc: float = (abs(min_height) + height) * 2 + 1
 	basis = basis.scaled(Vector3(1, calc, 1))
-	var transform = Transform(basis, pos)
+	var transform: Transform = Transform(basis, pos)
 	Terrain.multimesh.set_instance_transform(instance, transform)
-
+	return transform
 #func _on_noise_seed_changed(value: int) -> void:
 #	emit_signal("noise_changed", value)
 #func _on_forest_comparison_changed(value: float) -> void:
@@ -143,6 +154,10 @@ func _place_block(a: int, b: int, min_height:float, height: float,  noise: float
 #	emit_signal("forest_percent_changed", value)
 #func _on_forest_period_changed(value: int) -> void:
 #	emit_signal("forest_period_changed", value)
+
+# ######################################################################################## BLOCK GEN END
+
+
 
 func _on_refresh_ui(values) -> void:
 	emit_signal("refresh_ui", values)
@@ -154,4 +169,6 @@ func update_Terrain(isTragic: bool = false) -> void:
 
 func randomize() -> void:
 	MapVars.randomize()
-#	_generate(true)
+
+func export_map() -> Array:
+	return map_blocks
