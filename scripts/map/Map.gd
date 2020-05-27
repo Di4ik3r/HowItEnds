57eea99a8e3b7b468c3f87a5266acd521ecd04ba
Di4ik3r <di4iker@gmail.com>
2020-05-27 19:05:42 +0300

    map export: types to 2d array-map - implemented

--- scripts/map/Map.gd
+++ scripts/map/Map.gd
@@ -12,6 +12,7 @@ var map: Array
 onready var BlocksMM = get_node(BlocksMMPath)
 onready var TreesMM1 = get_node(TreesMM1Path)
 onready var TreesMM2 = get_node(TreesMM2Path)
+onready var Debug = $DebugInstance
 
 func _ready():
 	pass
@@ -22,18 +23,21 @@ func _set_resource(value: MapExport) -> void:
 	mapBuilder.generate_map()
 	map = mapBuilder.gen_2dim_map()
 	
+	
 	export_map_to_csv()
 
 func export_map_to_csv() -> void:
 	var blocks = resource.blocks
+	var divider = resource.b_side
 	var file = File.new()
 	file.open("user://map_export.csv", File.WRITE)
 	
 	var array = []
 	var buff = []
 	for i in range(blocks.size()):
-		buff.append(str(blocks[i].transform.origin.y) + ",")
-		if i % 100 == 0 and i != 0:
+		buff.append(str(blocks[i].type) + ",")
+		if (i + 1) % divider == 0:
+			print(buff)
 			file.store_csv_line(PoolStringArray(buff))
 			buff.clear()
 	
